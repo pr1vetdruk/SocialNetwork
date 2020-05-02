5a35f91bface26689d50dc83f7b6a3606c9c3758
Dmitry Somsin <privetdruk.inc@gmail.com>
2020-05-02 23:27:47 +0300

    Добавлена тестовая болванка ресурса и компонента Vue.js для публикаций

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,22 @@
+let publicationApi = Vue.resource('/id{user}/publication{/id}');
+
+Vue.component('publication-row', {
+    props: ['publication'],
+    template: '<div><i>({{publication.id}})</i> {{publication.text}}</div>'
+});
+
+Vue.component('publications-list', {
+    props: ['publications'],
+    template: '<div><publication-row v-for="publication in publications" :key="publication.id" :publication="publication"/></div>',
+    created: function () {
+        publicationApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(publication => this.publications.push(publication))
+            )
+        )
+    }
+});
+
 let app = new Vue({
     el: '#app',
     template: '<publications-list :publications="publications"/>',
